671d66eed43e6cb43c76f3f5b9d0aff632b8cb39
jakefish18 <jakefish.work@gmail.com>
2024-02-23 16:49:38 +0500

    Add empty folders for output and logs

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ Base.metadata.create_all(engine)
 from src.parsers import KanjiParser, WKRadicalsParser
 
 wk_radical_parser = WKRadicalsParser()
-wk_radical_parser.run()
+wk_radical_parser.run(is_download_image=True)
 
 kanji_parser = KanjiParser()
 kanji_parser.run()
